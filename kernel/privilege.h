42eabf170b0f745a64e1d4e013ecd234ecd3adab
Snowfall99 <445117445@qq.com>
2021-12-01 22:37:03 +0800

    kernel/privilege: privileges declared in kernel

---
+++ kernel/privilege.h
@@ -0,0 +1,20 @@
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+
+#define MAX_PRIVILEGE_NUM 128
+
+struct Privilege {
+    char exe[128];
+    char target[128];
+    int tombstone;
+    int value;
+};
+
+static struct Privilege WritePrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege ReadPrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege OpenPrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege RmdirPrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege MkdirPrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege ChmodPrivilege[MAX_PRIVILEGE_NUM];
+static struct Privilege CreatPrivilege[MAX_PRIVILEGE_NUM];
