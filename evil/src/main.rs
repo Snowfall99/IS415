da8fa6b1638963915f6505aada1afd910365e9e4
Snowfall99 <445117445@qq.com>
2021-12-02 10:01:07 +0800

    evil/main.rs: evil test binary

--- evil/src/main.rs
+++ evil/src/main.rs
@@ -1,6 +1,10 @@
 extern crate clap;
 
 use clap::{App, Arg};
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::os::unix::fs::PermissionsExt;
+use std::io::prelude::*;
 
 fn main() {
     let cmd = App::new("Evil")
@@ -50,4 +54,94 @@ fn main() {
                 .short('t')
                 .about("target file for rmdir")))
         .get_matches();
+
+    match cmd.subcommand_name() {
+        Some("READ") => {
+            let read = read(); match read {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e), 
+            }
+        }
+        Some("WRITE") => {
+            let write = write(); match write {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Some("OPEN") => {
+            let open = open(); match open {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Some("CHMOD") => {
+            let chmod = chmod(); match chmod {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Some("CREAT") => {
+            let creat = creat(); match creat {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Some("MKDIR") => {
+            let mkdir = mkdir(); match mkdir {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Some("RMDIR") => {
+            let rmdir = rmdir(); match rmdir {
+                Ok(_) => println!("OK"),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        None => println!("No subcommand was used"),
+        _ => unreachable!(),
+    };
+}
+
+fn read() -> std::io::Result<()> {
+    let mut file = File::open("test.txt")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    assert_eq!(content, "hello world\n");
+    Ok(())
+}
+
+fn write() -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("test.txt")
+        .unwrap();
+    file.write(b"hi\n")?;
+    Ok(())
+}
+
+fn open() -> std::io::Result<()> {
+    let _file = File::open("test.txt")?;
+    Ok(())
+}
+
+fn creat() -> std::io::Result<()> {
+    let _file = File::create("new.txt")?;
+    Ok(())
+}
+
+fn chmod() -> std::io::Result<()> {
+    fs::set_permissions("test.txt", fs::Permissions::from_mode(0o644)).unwrap();
+    Ok(())
+}
+
+fn mkdir() -> std::io::Result<()> {
+    fs::create_dir("new")?;
+    Ok(())
+}
+
+fn rmdir() -> std::io::Result<()> {
+    fs::remove_dir("new")?;
+    Ok(())
 }
