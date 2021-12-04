8710e3e38e97ddc501e1f3ebacc48c85ba216296
Snowfall99 <445117445@qq.com>
2021-12-05 01:21:53 +0800

    evil/creat.c: used for creat test

---
+++ evil/creat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void creat_file(char * filename) {
+    if(creat(filename, 0755)<0) {
+        printf("create file %s failure!\n",filename);
+        exit(EXIT_FAILURE);
+    } else
+        printf("create file %s success!\n", filename);
+}
+
+int main() {
+    creat_file("new.txt");
+}
