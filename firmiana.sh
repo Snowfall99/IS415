b0a92a3509021281c193f148d1c8bd87e166b4b7
Snowfall99 <445117445@qq.com>
2021-12-05 21:46:20 +0800

    firmiana: acquire to run as root

--- firmiana.sh
+++ firmiana.sh
@@ -14,11 +14,11 @@ function write_test() {
   cd ./evil
   echo "test.txt content: " | cat test.txt
   echo -e "\033[033mForbid\033[0m evil write to txt"
-  firmiana ADD -e evil -t txt --write 0
+  sudo firmiana ADD -e evil -t txt --write 0
   ./evil WRITE
   echo "test content: " | cat test.txt
   echo -e "\033[33mAllow\033[0m evil write to txt"
-  firmiana UPDATE -e evil -t txt --write 1
+  sudo firmiana UPDATE -e evil -t txt --write 1
   ./evil WRITE
   echo "test.txt content: " | cat test.txt
   echo -e "\033[32mWrite test finished\033[0m"
@@ -28,10 +28,10 @@ function read_test() {
   echo -e "\033[32mTest read privilege\033[0m"
   cd ./evil
   echo -e "\033[33mForbid\033[0m evil read txt"
-  firmiana ADD -e evil -t txt --read 0
+  sudo firmiana ADD -e evil -t txt --read 0
   ./evil READ
   echo -e "\033[33mAllow\033[0m evil read txt"
-  firmiana UPDATE -e evil -t txt --read 1
+  sudo firmiana UPDATE -e evil -t txt --read 1
   ./evil READ
   echo -e "\033[32mRead test finished\033[0m"
 }
@@ -40,10 +40,10 @@ function open_test() {
   echo -e "\033[32mTest open privilege\033[0m"
   cd ./evil
   echo -e "\033[33mForbid\033[0m evil open test.txt"
-  firmiana ADD -e evil -t test.txt --open 0
+  sudo firmiana ADD -e evil -t test.txt --open 0
   ./evil OPEN
   echo -e "\033[33mAllow\033[0m evil open test.txt"
-  firmiana UPDATE -e evil -t test.txt --open 1
+  sudo firmiana UPDATE -e evil -t test.txt --open 1
   ./evil OPEN
   echo -e "\033[32mOpen test finished\033[0m"
 }
@@ -54,11 +54,11 @@ function mkdir_test() {
   pwd
   ls --color -la | grep new
   echo -e "\033[33mForbid\033[0m evil make directory"
-  firmiana ADD -e evil --mkdir 0
+  sudo firmiana ADD -e evil --mkdir 0
   ./evil MKDIR
   ls --color -la | grep new
   echo -e "\033[33mAllow\033[0m evil make directory"
-  firmiana UPDATE -e evil --mkdir 1
+  sudo firmiana UPDATE -e evil --mkdir 1
   ./evil MKDIR
   ls --color -la | grep new 
   echo -e "\033[32mMkdir test finished\033[0m"
@@ -69,11 +69,11 @@ function rmdir_test() {
   cd ./evil 
   ls --color -la | grep new
   echo -e "\033[33mForbid\033[0m evil remove directory"
-  firmiana ADD -e evil -t new --rmdir 0
+  sudo firmiana ADD -e evil -t new --rmdir 0
   ./evil RMDIR 
   ls --color -la | grep new
   echo -e "\033[33mAllow\033[0m evil remove directory"
-  firmiana UPDATE -e evil -t new --rmdir 1
+  sudo firmiana UPDATE -e evil -t new --rmdir 1
   ./evil RMDIR
   ls --color -la | grep new 
   echo -e "\033[32mRmdir test finished\033[0m"
@@ -84,11 +84,11 @@ function chmod_test() {
   cd ./evil
   ls --color -la | grep test.txt
   echo -e "\033[33mForbid\033[0m evil change mode of txt"
-  firmiana ADD -e evil -t txt --chmod 0
+  sudo firmiana ADD -e evil -t txt --chmod 0
   ./evil CHMOD
   ls --color -la | grep test.txt
   echo -e "\033[33mAllow\033[0m evil change mode of txt"
-  firmiana UPDATE -e evil -t txt --chmod 1
+  sudo firmiana UPDATE -e evil -t txt --chmod 1
   ./evil CHMOD
   ls --color -la | grep test.txt
   echo -e "\033[32mChmod test finished\033[0m"
@@ -99,11 +99,11 @@ function creat_test() {
   cd ./evil
   ls --color
   echo -e "\033[33mForbid\033[0m create txt file"
-  firmiana ADD -e creat -t txt --creat 0
+  sudo firmiana ADD -e creat -t txt --creat 0
   ./creat
   ls --color
   echo -e "\033[33mAllow\033[0m create txt file"
-  firmiana UPDATE -e creat -t txt --creat 1
+  sudo firmiana UPDATE -e creat -t txt --creat 1
   ./creat
   ls --color
   echo -e "\033[32mCreat test finished\033[0m"
